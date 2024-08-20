972ec6eea57f17dfad4350b8f36ff4074a0464f2
Rashmi Raj <rashmiraj7877@gmail.com>
2024-08-21 00:40:54 +0530

    Fixes typo

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,8 @@
 const Intro = ({ fullName }) => {
   return (
     <div className='intro'>
-      <h1>My name is ${fullName}</h1>
+      <h1>My name is {fullName}</h1>
+
       <p>I am an Intern at Intel.</p>
     </div>
   );
