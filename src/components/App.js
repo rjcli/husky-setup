1b8f6a652373dc16d8865f4302f66043008b023b
Rashmi Raj <rashmi.raj@intel.com>
2024-08-21 00:35:55 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Intro from './Intro';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <h1>Husky Setup</h1>
+      <Intro fullName='Rashmi Raj' />
+
+      <footer>Copyright | Rashmi Raj.</footer>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Intro.js
@@ -0,0 +1,10 @@
+const Intro = ({ fullName }) => {
+  return (
+    <div className='intro'>
+      <h1>My name is ${fullName}</h1>
+      <p>I am an Intern at Intel.</p>
+    </div>
+  );
+};
+
+export default Intro;
